fbeaaa809f3d7ab1829e708a2a0c99d07c556cb2
deekshitha-hub <74289608+deekshitha-hub@users.noreply.github.com>
2020-12-23 14:30:50 +0530

    Update emp.h

--- inc/emp.h
+++ inc/emp.h
@@ -1,8 +1,19 @@
-#ifndef EMP_H
-#define EMP_H
-#include <stdio.h> ///for input output functions like printf, scanf
-#include <stdlib.h>
-#include <conio.h>
-#include <windows.h> ///for windows related functions (not important)
-#include <string.h>  ///string operations
+#ifndef EMP_H_INCLUDED
+#define EMP_H_INCLUDED
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int i,j,main_exit;
+
+void menu();
+void new_entry();
+void edit();
+void view_list();
+void erase();
+void see();
+void sorter();
+void closer();
+
+#endif
